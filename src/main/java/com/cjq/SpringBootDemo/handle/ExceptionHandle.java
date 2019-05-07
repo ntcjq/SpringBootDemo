b9a3cc4eca9e01ee769953c50865bc98395ebb49
ntcjq <754119493@qq.com>
2019-05-07 18:12:55 +0800

    ExceptionHandler返回页面和JSON

--- src/main/java/com/cjq/SpringBootDemo/handle/ExceptionHandle.java
+++ src/main/java/com/cjq/SpringBootDemo/handle/ExceptionHandle.java
@@ -8,24 +8,48 @@ import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
 
 @ControllerAdvice
-@ResponseBody
 public class ExceptionHandle {
 
     private final static Logger logger = LoggerFactory.getLogger(ExceptionHandle.class);
 
 
-    @ExceptionHandler
-    public Result handle(Exception e){
-        if(e instanceof MyException){
-            MyException myException = (MyException)e;
-            logger.error("【自定义异常】{}",myException.getMessage());
-            return ResultUtil.faild(myException.getCode(),myException.getMessage());
+    /**
+     * 自定义异常返回JSON
+     * @param e
+     * @return
+     */
+    @ExceptionHandler(MyException.class)
+    @ResponseBody
+    public Result handle(Exception e) {
+        if (e instanceof MyException) {
+            MyException myException = (MyException) e;
+            logger.error("【自定义异常】{}", myException.getMessage());
+            return ResultUtil.faild(myException.getCode(), myException.getMessage());
         }
-        logger.error("【系统异常】{}",e.getMessage());
+        logger.error("【系统异常】{}", e.getMessage());
         e.printStackTrace();
-        return ResultUtil.faild(500,e.getMessage());
+        return ResultUtil.faild(500, e.getMessage());
+    }
+
+    /**
+     * 非自定义异常返回错误页面
+     * @param req
+     * @param e
+     * @return
+     * @throws Exception
+     */
+    @ExceptionHandler(value = Exception.class)
+    public ModelAndView defaultErrorHandler(HttpServletRequest req, Exception e) throws Exception {
+        ModelAndView mav = new ModelAndView();
+        mav.addObject("exception", e);
+        mav.addObject("url", req.getRequestURL());
+        mav.setViewName("/error");
+        return mav;
     }
 
 }
--- src/test/java/com/cjq/SpringBootDemo/Test.java
+++ src/test/java/com/cjq/SpringBootDemo/Test.java
@@ -12,9 +12,14 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import javax.naming.ldap.PagedResultsControl;
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
+import java.math.BigDecimal;
+import java.time.LocalDate;
 import java.util.*;
 import java.util.concurrent.*;
 import java.util.concurrent.locks.ReentrantLock;
+import java.util.function.Supplier;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import static javafx.scene.input.KeyCode.L;
 
@@ -60,9 +65,75 @@ public class Test {
 //        }
 //        executorService.shutdown();
 
+        Random random = new Random();
+        String[] passwords = {"A","B","C"};
+        List<User> list = new ArrayList<>();
+        for(int i=0;i<10;i++){
+            User user = new User();
+            user.setUserName("name"+i);
+            user.setId((long)random.nextInt(1000));
+            user.setPassWord(passwords[random.nextInt(3)]);
+            list.add(user);
+        }
+        List<Map> collect = list.stream().map(user -> {
+            Map map = new HashMap();
+            map.put("username", user.getUserName());
+            map.put("password", user.getPassWord());
+            return map;
+        }).filter(user-> user != null).collect(Collectors.toList());
+
+        long sum = list.stream().mapToLong(user -> user.getId()).sum();
+        long sum2 = list.stream().mapToLong(User::getId).sum();
+        System.out.println(sum);
+        System.out.println(sum2);
 
 
+        Stream<BigDecimal> bigDecimalStream = Stream.of(null,new BigDecimal(100), new BigDecimal(102), new BigDecimal(103), new BigDecimal(104));
+        BigDecimal bigDecimal = bigDecimalStream.filter(x -> x != null).reduce(BigDecimal::add).get();
+
+
+        Map<String, List<User>> collect1 = list.stream().collect(Collectors.groupingBy(User::getPassWord));
+
+
+        for(Map.Entry<String,List<User>> listEntry : collect1.entrySet()){
+            String key = listEntry.getKey();
+            List<User> value = listEntry.getValue();
+        }
+
+
+        collect1.forEach((k,v) -> System.out.println(k+":"+v));
+
+
+        Stream<Double> generate = Stream.generate(new Supplier<Double>() {
+
+            @Override
+            public Double get() {
+                return Math.random();
+            }
+        });
+        System.out.println(generate);
+        List<Double> collect2 = generate.skip(99).limit(100).collect(Collectors.toList());
+        System.out.println(collect2);
+
+        test1();
 
     }
 
+    public static void test1(){
+        Date date=new Date();
+        //c的使用
+        System.out.printf("全部日期和时间信息：%tc%n",date);
+        //f的使用
+        System.out.printf("年-月-日格式：%tF%n",date);
+        //d的使用
+        System.out.printf("月/日/年格式：%tD%n",date);
+        //r的使用
+        System.out.printf("HH:MM:SS PM格式（12时制）：%tr%n",date);
+        //t的使用
+        System.out.printf("HH:MM:SS格式（24时制）：%tT%n", date);
+        //R的使用
+        System.out.printf("HH:MM格式（24时制）：%tR%n",date);
+
+        System.out.println(String.format("%tF",date).replaceAll("-",""));
+    }
 }
