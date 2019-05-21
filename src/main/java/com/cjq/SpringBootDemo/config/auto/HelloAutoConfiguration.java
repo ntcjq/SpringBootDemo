6ea6319c3e6f0480a93149b85df5562137c1bba0
ntcjq <754119493@qq.com>
2019-05-21 11:11:48 +0800

    详细说明

--- src/main/java/com/cjq/SpringBootDemo/config/auto/HelloAutoConfiguration.java
+++ src/main/java/com/cjq/SpringBootDemo/config/auto/HelloAutoConfiguration.java
@@ -10,7 +10,10 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 //这里就是前面说的，这个注解读入我们的配置对象类
 @EnableConfigurationProperties(HelloProperties.class)
-//当类路径存在这个类时才会加载这个配置类，否则跳过,这个很有用比如不同jar包间类依赖，依赖的类不存在直接跳过，不会报错
+//@ConditionalOnClass注解存在的意义在于判断类加载器中是否存在HelloService这个类，如果存在的话会在Spring容器中加载HelloAutoConfiguration配置类；否则不会加载。
+//注意：上面说的是在Spring容器中加载HelloAutoConfiguration这个配置类，不是在Spring容器中加载HelloService这个类。
+//两种写法：
+//@ConditionalOnClass(name="com.cjq.SpringBootDemo.config.auto.HelloService")
 @ConditionalOnClass(HelloService.class)
 public class HelloAutoConfiguration {
 
@@ -18,7 +21,7 @@ public class HelloAutoConfiguration {
     private HelloProperties helloProperties;
 
     @Bean
-    //这个配置就是SpringBoot可以优先使用自定义Bean的核心所在，如果没有我们的自定义Bean才会像下面这样去自动配置一个新的Bean
+    //这个配置就是SpringBoot可以优先使用自定义Bean的核心所在，如果Spring容器中没有我们的自定义Bean才会像下面这样去创建一个新的Bean
     @ConditionalOnMissingBean(HelloService.class)
     public HelloService auto(){
         HelloService helloService =new HelloService();
